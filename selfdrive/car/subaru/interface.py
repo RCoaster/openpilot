9425a33de62facd5e6c80f70317eb41e4248757e
RCoaster <75134639+RCoaster@users.noreply.github.com>
2022-04-20 05:20:02 -0400

    Update interface.py

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -30,10 +30,10 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.89
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 13.5
-      ret.steerActuatorDelay = 0.3   # end-to-end angle controller
+      ret.steerActuatorDelay = 0.25   # end-to-end angle controller
       ret.lateralTuning.pid.kf = 0.00003
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20., 40.], [0., 20., 40.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.0025, 0.0925, .1975], [0.00025, 0.00925, .01975]]
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 40.], [0., 40.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.0025, .212625], [0.00025, .0212625]]
 
     if candidate == CAR.IMPREZA:
       ret.mass = 1568. + STD_CARGO_KG
