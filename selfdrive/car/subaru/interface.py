cf7bc8682b152c5826266220124878701623aad4
RCoaster <75134639+RCoaster@users.noreply.github.com>
2023-07-30 15:26:01 -0400

    Update interface.py

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -33,15 +33,15 @@ class CarInterface(CarInterfaceBase):
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     if candidate == CAR.ASCENT:
-      ret.mass = 2031. + STD_CARGO_KG
-      ret.wheelbase = 2.89
-      ret.centerToFront = ret.wheelbase * 0.5
-      ret.steerRatio = 13.5
-      ret.steerActuatorDelay = 0.3   # end-to-end angle controller
-      ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kf = 0.00003
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.0025, 0.1], [0.00025, 0.01]]
+    ret.mass = 2231. + STD_CARGO_KG   # stock OP 2031. + STD_CARGO_KG
+    ret.wheelbase = 2.89
+    ret.centerToFront = ret.wheelbase * 0.5
+    ret.steerRatio = 13.5
+    ret.steerActuatorDelay = 0.34   # end-to-end angle controller, stock OP 0.3
+    ret.lateralTuning.init('pid')
+    ret.lateralTuning.pid.kf = 0.000041     # stock OP 0.00003
+    ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]                   # stock OP [[0., 20.], [0.,20.]]
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.02, 0.19], [0.0042, 0.028]]   # stock OP [[0.0025, 0.1], [0.00025, 0.01]]
 
     elif candidate == CAR.IMPREZA:
       ret.mass = 1568. + STD_CARGO_KG
